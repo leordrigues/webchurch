9dec46d4148a23bbc146d962d1e26ca0f8ef5739
leordrigues <leochuckdk@gmail.com>
2019-06-07 15:45:46 -0300

    Testando commit 2

--- lib/detail_bottom.dart
+++ lib/detail_bottom.dart
@@ -12,7 +12,7 @@ class DetailBottom extends StatelessWidget {
               padding: const EdgeInsets.only(bottom: 50,),            
               scrollDirection: Axis.horizontal,
               children: <Widget>[ 
-                Group(title: "Oi",color: Colors.orange, ),
+                Group(title: "Josi",color: Colors.orange, ),
                 Group(title: "Estou",color: Colors.green,),
                 Group(title: "Testando",color: Colors.indigo,),
                 Group(title: "O App",color: Colors.blue,),

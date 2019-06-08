e23397277ba2ff30888e3467d4877f95f82f94bd
Leonardo <leo.rdrigues@live.com>
2019-06-08 00:57:05 -0300

    Commit image

--- lib/Horizonta_listView.dart
+++ lib/Horizonta_listView.dart
@@ -1,23 +1,25 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class MainScreen extends StatelessWidget {
-  final List<int> numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
+  final List<int> numbers = [1, 2, 3, 4, 5, 13, 21, 34, 55];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-    //  appBar: AppBar(
-      //  centerTitle: true,        
-        backgroundColor: Color(0xFFDAE8F5), //coloquei uma cor no appbar -forçada
-        //title: Text(
-          //'Aplicativo Igreja',                  
-          //textAlign: TextAlign.center,
-        //),
+    return Scaffold(
+      //  appBar: AppBar(
+      //  centerTitle: true,
+      backgroundColor: Color(0xFFDAE8F5), //coloquei uma cor no appbar -forçada
+      //title: Text(
+      //'Aplicativo Igreja',
+      //textAlign: TextAlign.center,
+      //),
       //),
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 30.0),
-        height: MediaQuery.of(context).size.height * 0.66, 
+        height: MediaQuery.of(context).size.height * 0.65,
         child: ListView.builder(
             scrollDirection: Axis.horizontal,
             itemCount: numbers.length,
@@ -26,14 +28,14 @@ class MainScreen extends StatelessWidget {
                 width: MediaQuery.of(context).size.width * 0.9,
                 child: Card(
                   shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(19.0)), //circular o Container
-                  color: Color(0xFF4E5685), //quero essa cor #4E5685                
+                      borderRadius:
+                          BorderRadius.circular(19.0)), //circular o Container
+                  color: Color(0xFF4E5685), //quero essa cor #4E5685
                   child: Container(
-                    child: Center(
-                        child: Text(
-                      numbers[index].toString(),
-                      style: TextStyle(color: Colors.white, fontSize: 36.0),
-                    )),
+                    //coloar imagem externa
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("assets/imag1.jpeg"))),
                   ),
                 ),
               );

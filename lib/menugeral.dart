701ddbb504b255104931c8ecec00270120fb89d2
Leonardo <leochuckdk@gmail.com>
2019-06-12 13:06:24 -0300

    Today
    Today

--- lib/menugeral.dart
+++ lib/menugeral.dart
@@ -1,16 +1,27 @@
+import 'package:app_igreja/mainmenu.dart';
 import 'package:app_igreja/versiculo.dart';
 import 'package:flutter/material.dart';
 
 class Menugeral extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      child: Container(
-        width: 200,
-        height: 200,
-        child: Versiculo(),
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(flex: 2,
+        child: MainMenu(),//chamei a classe dos grid
+        ),
+SizedBox(height: 10.0,),
+Expanded(flex: 1,
+child: Container( 
+  padding: EdgeInsets.all(30), 
+  child: Text("ola estou testando"),
+  color: Colors.yellow,
+  height: 50,
+  width: 50,
+),
+)
+      ],
     );
   }
 }
--- lib/versiculo.dart
+++ lib/versiculo.dart
@@ -4,10 +4,16 @@ import 'package:flutter/material.dart' as prefix0;
 class Versiculo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      
-        textColor: Colors.white, color: Colors.purple, onPressed: () {});
-        
-
+    return Center(
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+            color: Colors.cyan,
+            borderRadius: BorderRadius.all (Radius.circular(10.0))),
+            alignment: Alignment.topLeft,
+            child: Text("oi noix"),
+      ),
+    );
   }
 }
